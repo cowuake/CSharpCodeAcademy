10649574a972c803f40b09637e215ce7ecf5e426
Riccardo Mura <riccardo_mura@yahoo.com>
2022-03-07 15:51:45 +0100

    Two more examples

---
+++ HelloWorld/AveragePlus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AveragePlus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Spawn user prompt
+            Console.Write("How many number do you want to insert?   ");
+
+            // Store the number of expected inputs
+            int nNumbers = int.Parse(Console.ReadLine());
+
+            // Initialize variable for the sum
+            int sum = 0;
+
+            // Take inputs and increment the sum
+            for (int i = 1; i <= nNumbers; i++)
+            {
+                Console.Write($"Please insert number {i}:   ");
+                sum += int.Parse(Console.ReadLine());           
+            }
+
+            // Print output to the console
+            Console.WriteLine($"The sum of all numbers is {sum}!");
+            Console.WriteLine($"The average of all numbers is {(float)sum / nNumbers}!");
+        }
+    }
+}
---
+++ HelloWorld/Avg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Avg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Compute average ***");
+
+            Console.Write("Insert first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Insert second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Average: {(firstNumber + secondNumber)/2.0f}");
+        }
+    }
+}
