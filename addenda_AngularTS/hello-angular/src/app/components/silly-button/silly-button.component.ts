800053d0b9d1c7130e66045108c7ec9639ac4000
Riccardo Mura <riccardo_mura@yahoo.com>
2022-05-16 18:29:05 +0200

    Enough for today

--- addenda_AngularTS/hello-angular/src/app/components/silly-button/silly-button.component.ts
+++ addenda_AngularTS/hello-angular/src/app/components/silly-button/silly-button.component.ts
@@ -4,21 +4,19 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-silly-button',
   templateUrl: './silly-button.component.html',
-  styleUrls: ['./silly-button.component.css']
+  styleUrls: ['./silly-button.component.css'],
 })
 export class SillyButtonComponent implements OnInit {
   @Input() text!: string;
   @Input() color!: string;
   @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onClick() {
     //this.btnClick.emit();
-    window.open("https://www.youtube.com/watch?v=I4Q3YDezqcM", "_blank");
+    window.open('https://www.youtube.com/watch?v=I4Q3YDezqcM', '_blank');
   }
-
 }
