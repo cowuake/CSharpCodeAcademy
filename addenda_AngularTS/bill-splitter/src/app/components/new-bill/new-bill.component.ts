e42d3ffd068bbc26c73557117d732f5982847a70
Riccardo Mura <riccardo_mura@yahoo.com>
2022-07-06 07:21:18 +0200

    I want to spliiiit biills!

--- addenda_AngularTS/bill-splitter/src/app/components/new-bill/new-bill.component.ts
+++ addenda_AngularTS/bill-splitter/src/app/components/new-bill/new-bill.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ITenant } from 'src/app/ITenant';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-new-bill',
   templateUrl: './new-bill.component.html',
-  styleUrls: ['./new-bill.component.css']
+  styleUrls: ['./new-bill.component.css'],
 })
 export class NewBillComponent implements OnInit {
+  name?: string = '';
+  firstDay: Date = new Date();
+  lastDay: Date = new Date();
+  amount: number = 0;
+  tenants: ITenant[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  OnSubmit() {
+    const newBill = {
+      name: this.name,
+      firstDay: this.firstDay,
+      lastDay: this.lastDay,
+      amount: this.amount,
+      tenant: this.tenants,
+    };
+
+    this.name = '';
+    this.firstDay = new Date();
+    this.lastDay = new Date();
+    this.amount = 0;
+    this.tenants = [];
   }
-
 }
