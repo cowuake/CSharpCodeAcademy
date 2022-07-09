0e91330a1ca878e25772dc4b68d497643280ada9
r-mura <riccardo_mura@yahoo.com>
2022-07-09 19:00:12 +0200

    Hello, Material!

--- addenda_AngularTS/bill-splitter/src/app/app.module.ts
+++ addenda_AngularTS/bill-splitter/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewBillComponent } from './components/new-bill/new-bill.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NewBillComponent
-  ],
+  declarations: [AppComponent, NewBillComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
