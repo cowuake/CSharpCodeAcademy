83136ade35ee69b2ad41759b1d861a11cff6d69a
r-mura <riccardo_mura@yahoo.com>
2022-11-20 11:48:53 +0100

    Solved error about transient animations

--- addenda_AngularTS/bill-splitter/src/app/app.module.ts
+++ addenda_AngularTS/bill-splitter/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { NewBillComponent } from './components/new-bill/new-bill.component';
   declarations: [AppComponent, NewBillComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     MatButtonModule,
