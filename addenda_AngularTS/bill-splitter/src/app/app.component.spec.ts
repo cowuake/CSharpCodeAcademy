6afdff39d795902c4bca5bf46488725d24c7a80a
r-mura <riccardo_mura@yahoo.com>
2022-11-20 11:12:36 +0100

    Updated packages

--- addenda_AngularTS/bill-splitter/src/app/app.component.spec.ts
+++ addenda_AngularTS/bill-splitter/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        NoopAnimationsModule
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent
